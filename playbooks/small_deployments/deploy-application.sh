ce245d7ef5e928b7d6f9b153b2d724a9182c2e14
kuznetsov.danil <kuznetsov.danil.v@gmail.com>
2016-12-08 13:28:30 +0200

    refactore run script and add secure repository

--- playbooks/small_deployments/deploy-application.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-##
-## Run the deploy-application.yml playbook in the configuration/playbooks directory
-##
-cd /var/tmp/configuration/playbooks/small_deployments/  && sudo ansible-playbook -c local ./deploy-application.yml -i "localhost," -e@ansible_overrides.yml
--- playbooks/small_deployments/deploy-common.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-##
-## Run the deploy-common.yml playbook in the configuration/playbooks directory
-##
-cd /var/tmp/configuration/playbooks/small_deployments/  && sudo ansible-playbook -c local ./deploy-common.yml -i "localhost," -e@ansible_overrides.yml
